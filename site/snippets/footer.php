ad450c708bdd0a4fcb9a21367870ddd9674aad02
ModernAmusements <st@zahnarzt-helden.de>
2020-07-20 12:11:23 +0200

    clean footer

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,12 +1,3 @@
-<?php
-/**
- * Snippets are a great way to store code snippets for reuse or to keep your templates clean.
- * in loops or simply to keep your templates clean.
- * This footer snippet is reused in all templates. In fetches information from the `site.txt` content file
- * and from the `about` page.
- * More about snippets: https://getkirby.com/docs/guide/templates/snippets
- */
-?>
 <div class="grid">
 <footer>
   <h3>
@@ -17,22 +8,9 @@
   </div>
   </a>
 </footer>
-
-
-
-
-
 </div>
-
-
-
-
-
 <?= js('assets/js/libs.js') ?>
 <?= js('assets/js/scripts.js') ?>
 <?= js('assets/js/main.js') ?>
-
-
-
 </body>
 </html>

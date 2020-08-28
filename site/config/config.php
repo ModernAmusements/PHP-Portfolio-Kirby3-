007d654c73486fae496bf59ad66440ccc87db19d
ModernAmusements <st@zahnarzt-helden.de>
2020-08-28 22:12:03 +0200

    smtp config for gmail test

--- site/config/config.php
+++ site/config/config.php
@@ -1,15 +1,18 @@
 <?php
-
-/**
- * The config file is optional. It accepts a return array with config options
- * Note: Never include more than one return statement, all options go within this single return array
- * In this example, we set debugging to true, so that errors are displayed onscreen. 
- * This setting must be set to false in production.
- * All config options: https://getkirby.com/docs/reference/system/options
- */
 return [
     'debug' => true,
     'panel' =>[
       'install' => true
+    ],
+    'email' => [
+      'transport' => [
+        'type' => 'smtp',
+        'host' => 'smtp.gmail.com',
+        'port' => 465,
+        'security' => true,
+        'auth' => true,
+        'username' => 'Shady Tawfik',
+        'password' => '4G7qasdf',
+      ]
     ]
 ];

c4a15a3eb46a4a0884d6b60eb5ce4de86033b787
ModernAmusements <st@zahnarzt-helden.de>
2020-11-08 16:36:52 +0100

    test Gyro

--- assets/js/homePageScripts.js
+++ assets/js/homePageScripts.js
@@ -10997,14 +10997,37 @@ $(function () {
   }
 });
 $(function () {
+  var gyro = false;
+
+  if (DeviceMotionEvent && typeof DeviceMotionEvent.requestPermission === "function") {
+    DeviceMotionEvent.requestPermission().then(function (response) {
+      if (response == 'granted') {
+        window.addEventListener('devicemotion', function (event) {
+          if (event.rotationRate.alpha || event.rotationRate.beta || event.rotationRate.gamma) gyro = true;
+        });
+      }
+    })["catch"](console.error);
+  }
+
   var card = $(".card");
-  card.on("mousemove", function (e) {
-    var x = e.clientX - $(this).offset().left + $(window).scrollLeft();
-    var y = e.clientY - $(this).offset().top + $(window).scrollTop();
-    var rY = map(x, 0, $(this).width(), -17, 17);
-    var rX = map(y, 0, $(this).height(), -17, 17);
-    $(this).children(".image").css("transform", "rotateY(" + rY + "deg)" + " " + "rotateX(" + -rX + "deg)");
-  });
+
+  if (gyro) {
+    if (accx != undefined) {
+      globalX = globalX + accx * 2;
+    }
+
+    if (accy != undefined) {
+      globalY = globalY - accy * 2;
+    }
+  } else {
+    card.on("mousemove", function (e) {
+      var x = e.clientX - $(this).offset().left + $(window).scrollLeft();
+      var y = e.clientY - $(this).offset().top + $(window).scrollTop();
+      var rY = map(x, 0, $(this).width(), -17, 17);
+      var rX = map(y, 0, $(this).height(), -17, 17);
+      $(this).children(".image").css("transform", "rotateY(" + rY + "deg)" + " " + "rotateX(" + -rX + "deg)");
+    });
+  }
 
   function map(x, in_min, in_max, out_min, out_max) {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
--- src/js/01_homePageScripts.js
+++ src/js/01_homePageScripts.js
@@ -21,22 +21,50 @@ $(function() {
 });
 
 $(function () {
+
+  let gyro = false;
+
+  if ( DeviceMotionEvent && typeof DeviceMotionEvent.requestPermission === "function") {
+
+      DeviceMotionEvent.requestPermission()
+      .then(response => {
+          if (response == 'granted') {
+              window.addEventListener('devicemotion', (event) => {
+                  if(event.rotationRate.alpha || event.rotationRate.beta || event.rotationRate.gamma)
+                  gyro = true;
+              })
+          }
+      })
+      .catch(console.error)
+  }
+
   var card = $(".card");
-  card.on("mousemove", function (e) {
-    var x =
-      e.clientX - $(this).offset().left + $(window).scrollLeft();
-    var y = e.clientY - $(this).offset().top + $(window).scrollTop();
 
-    var rY = map(x, 0, $(this).width(), -17, 17);
-    var rX = map(y, 0, $(this).height(), -17, 17);
+  if(gyro){
+    if(accx != undefined){
+      globalX = globalX+accx*2;
+            }
+    if(accy != undefined){
+      globalY = globalY-accy*2;
+    }
+  } else {
+    card.on("mousemove", function (e) {
+      var x =
+        e.clientX - $(this).offset().left + $(window).scrollLeft();
+      var y = e.clientY - $(this).offset().top + $(window).scrollTop();
 
-    $(this)
-      .children(".image")
-      .css(
-        "transform",
-        "rotateY(" + rY + "deg)" + " " + "rotateX(" + -rX + "deg)"
-      );
-  });
+      var rY = map(x, 0, $(this).width(), -17, 17);
+      var rX = map(y, 0, $(this).height(), -17, 17);
+
+      $(this)
+        .children(".image")
+        .css(
+          "transform",
+          "rotateY(" + rY + "deg)" + " " + "rotateX(" + -rX + "deg)"
+        );
+    });
+  }
+
 
   function map(x, in_min, in_max, out_min, out_max) {
     return (
@@ -45,3 +73,7 @@ $(function () {
     );
   }
 });
+
+
+
+
